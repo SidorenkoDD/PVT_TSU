68b5cc1a58913a8eb07b3eb0e50675e3f1375aff
Sidorenko Daniil <147086850+SidorenkoDD@users.noreply.github.com>
2025-09-09 15:25:51 +0300

    промежуточный коммит

--- code/calculations/CompositionalModel/CompositionalModel.py
+++ code/calculations/CompositionalModel/CompositionalModel.py
@@ -10,6 +10,7 @@ from calculations.Composition.Composition import Composition
 from calculations.VLE.Flash import FlashFactory
 from calculations.Utils.Conditions import Conditions
 from calculations.PhaseDiagram.PhaseDiagram_v4 import PhaseDiagram
+from calculations.CompositionalModel.Variant import CompositionalVariantFactory
 
 
 
@@ -18,24 +19,17 @@ class CompositionalModel:
         self._composition = zi
         self._eos = eos
         self._flash_results = {}
+        self._compositional_variants_factory = CompositionalVariantFactory()
+        self._compositional_variants = {}
+        self._create_base_variant()
 
-    def flash(self, conditions, flash_type = 'TwoPhaseFlash'):
-        self._flash_object = FlashFactory(self._composition, self._eos)
-        flash_calculator = self._flash_object.create_flash(flash_type=flash_type)
-        result = flash_calculator.calculate(conditions=conditions)
+    def _create_base_variant(self):
+        '''Creates base compositional variant'''
+        self._compositional_variants['base'] = self._compositional_variants_factory.create_variant(self._composition, 'PREOS')
 
-        self._flash_results[str(flash_type) + '_' + str(conditions.p)+'_' + str(conditions.t)] = result 
-    
-    
-    def plot_phase_diagram(self, p_max = 40, t_min = 0, t_max = 200, t_step = 10):
-        self.phase_diagram_obj = PhaseDiagram(self._composition, p_max= p_max, t_min= t_min, t_max= t_max, t_step= t_step)
-        self.phase_diagram_obj.calc_phase_diagram(eos = self._eos)
-        self.phase_diagram_obj.plot_phase_diagram()
+    def create_variant(self, name, composition, eos):
+        self._compositional_variants[name] = self._compositional_variants_factory.create_variant(composition, eos)
 
-
-    @property
-    def show_flashes(self):
-        return self._flash_results
 
 
 if __name__ == '__main__':
--- code/calculations/CompositionalModel/Variant.py
+++ code/calculations/CompositionalModel/Variant.py
@@ -1,11 +1,49 @@
 from calculations.Composition.Composition import Composition
+from calculations.VLE.Flash import FlashFactory
+from calculations.PhaseDiagram.PhaseDiagram_v4 import PhaseDiagram
 #from calculations.EOS import BaseEOS
 
+class CompositionalVariantFactory:
+    def __init__(self):
+        self.counter = 0
 
-class Variant:
-    def __init__(self, name, composition: Composition, eos:str):
-        self.name = name
-        self.composition = composition
-        self.eos = eos
+    def create_variant(self, composition, eos):
+        self.counter +=1
+        return CompositionalVariant(f'Variant_{self.counter}',
+                                    composition,
+                                     eos)
+
+
+class CompositionalVariant:
+
+    def __init__(self, zi: Composition, eos: str = 'PREOS'):
+        self._composition = zi
+        self._eos = eos
+        self._flash_results = {}
+        self._compositional_variants_factory = CompositionalVariantFactory()
+        self.compositional_variants = {}
+
+    def flash(self, conditions, flash_type = 'TwoPhaseFlash'):
+        self._flash_object = FlashFactory(self._composition, self._eos)
+        flash_calculator = self._flash_object.create_flash(flash_type=flash_type)
+        result = flash_calculator.calculate(conditions=conditions)
+
+        self._flash_results[str(flash_type) + '_' + str(conditions.p)+'_' + str(conditions.t)] = result 
+    
+    
+    def plot_phase_diagram(self, p_max = 40, t_min = 0, t_max = 200, t_step = 10):
+        self.phase_diagram_obj = PhaseDiagram(self._composition, p_max= p_max, t_min= t_min, t_max= t_max, t_step= t_step)
+        self.phase_diagram_obj.calc_phase_diagram(eos = self._eos)
+        self.phase_diagram_obj.plot_phase_diagram()
+
+
+class CompositionalVariantFactory:
+    def __init__(self):
+        self.counter = 0
+
+    def create_variant(self, composition, eos):
+        self.counter +=1
+        return CompositionalVariant(composition,
+                                     eos)
 
     
